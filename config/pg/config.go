25cf3dccb8c8ad275684fe386b16a6d6600f0bb0
Matthias Luebken <matthias.luebken@gmail.com>
2022-11-01 13:39:41 +0100

    aiven_pg
    Signed-off-by: Matthias Luebken <matthias.luebken@gmail.com>

--- config/pg/config.go
+++ config/pg/config.go
@@ -4,7 +4,7 @@ import "github.com/upbound/upjet/pkg/config"
 
 // Configure configures individual resources by adding custom ResourceConfigurators.
 func Configure(p *config.Provider) {
-	p.AddResourceConfigurator("github_repository", func(r *config.Resource) {
+	p.AddResourceConfigurator("aiven_pg", func(r *config.Resource) {
 		// We need to override the default group that upjet generated for
 		// this resource
 		r.ShortGroup = "pg"
